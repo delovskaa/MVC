a668edc04741d5042f613de7dc9ffc4a33033a7a
Aleksandra Delovska <94134821+delovskaa@users.noreply.github.com>
2022-12-12 15:52:53 +0100

    Add project files.

---
+++ Lab5MVC/Controllers/FriendModelsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab5MVC.Models;
+
+namespace Lab5MVC.Controllers
+{
+    public class FriendModelsController : Controller
+    {
+        private FriendContext db = new FriendContext();
+
+        // GET: FriendModels
+        public ActionResult Index()
+        {
+            return View(db.friends.ToList());
+        }
+
+        // GET: FriendModels/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendModel friendModel = db.friends.Find(id);
+            if (friendModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendModel);
+        }
+
+        // GET: FriendModels/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: FriendModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,FriendID,Name,Hometown")] FriendModel friendModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.friends.Add(friendModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(friendModel);
+        }
+
+        // GET: FriendModels/Edit/5
+        [Authorize(Roles = "Administrator,Manager")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendModel friendModel = db.friends.Find(id);
+            if (friendModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendModel);
+        }
+
+        // POST: FriendModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,FriendID,Name,Hometown")] FriendModel friendModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friendModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(friendModel);
+        }
+
+        // GET: FriendModels/Delete/5
+        [Authorize(Roles = "Administrator")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FriendModel friendModel = db.friends.Find(id);
+            if (friendModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friendModel);
+        }
+
+        // POST: FriendModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            FriendModel friendModel = db.friends.Find(id);
+            db.friends.Remove(friendModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Lab5MVC/Models/FriendModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab5MVC.Models
+{
+    public class FriendModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Range(0, 200, ErrorMessage = "FriendID must be between 0-200")]
+        [Display(Name = "Friend ID")]
+        public int FriendID { get; set; }
+
+        [Required] 
+        public string Name { get; set; }
+        [Required] 
+        public string Hometown { get; set; }
+    }
+}
